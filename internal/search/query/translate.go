c1100adbcd42601d5c013b012b5f7f76b00cf58e
Rijnard van Tonder <rvantonder@gmail.com>
2021-02-26 19:37:20 -0700

    wip

--- internal/search/query/translate.go
+++ internal/search/query/translate.go
@@ -3,16 +3,24 @@ package query
 import "github.com/sourcegraph/sourcegraph/cmd/frontend/envvar"
 
 func toRepoQuery(q Q) (*RepoQuery, bool) {
-	return nil, false
+	includeRepos, excludeRepos := FindFields(q, "repo")            // enforce isNotNegated
+	includeRepoGroups := FindField(q, "repogroup")                 // enforce singular, isNotNegated
+	fork := ParseYesNoOnly(FindPositiveField(q, "fork"))           // enforce singular
+	archived := ParseYesNoOnly(FindPositiveField(q, "archived"))   // enforce singular
+	privacy := ParseVisibility(FindPositiveField(q, "visibility")) // ... etc..
+	// TODO: deal with repoHasCommitAfter.
+	contextID := FindPositiveField(q, "context")
+	// TODO: version context from URL parameter.
 
+	return nil, false
 }
 
 func translate(q Q) InternalQuery {
-	if _, ok := toRepoQuery(q); ok {
+	if repoQuery, ok := toRepoQuery(q); ok {
 		if envvar.SourcegraphDotComMode() {
 			return RepoQuery{DotComDefault{}}
 		}
-		return RepoQuery{Subset{}}
+		return repoQuery
 	}
 	return GenericQuery{q}
 }
